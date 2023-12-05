460b8ede49fd104d7c157f8fd165eae0c14cc3be
Lucy Tran <ltran2@macalester.edu>
2023-12-05 15:50:11 -0600

    Added getUserByUsername() and functions' return type

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -1,19 +1,80 @@
 import type { Password, User } from "@prisma/client";
+import { GetResult } from "@prisma/client/runtime";
 import bcrypt from "bcryptjs";
+import { matchSorter } from "match-sorter";
 
 import { prisma } from "~/db.server";
 
 export type { User } from "@prisma/client";
 
-export async function getUserById(id: User["id"]) {
-  return prisma.user.findUnique({ where: { id } });
+type UserResult = {
+  id: number;
+  firstName?: string | null;
+  lastName?: string | null;
+  name?: string | null;
+  email: string;
+  profilePic?: string | null; // shown on the landing page
+  bio?: string | null; // used as the big title for the landing page
+  intro?: string | null; // used as the big title for the landing page
+};
+
+export async function getUsers(
+  query?: string | null,
+): Promise<UserResult[] | null> {
+  let users = await prisma.user.findMany({
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+      name: true,
+      email: true,
+      profilePic: true,
+      bio: true,
+      intro: true,
+    },
+    orderBy: { updatedAt: "desc" },
+  });
+  if (query) {
+    users = matchSorter(users, query, {
+      keys: ["firstName", "lastName", "name", "email"],
+    });
+  }
+  return users;
 }
 
-export async function getUserByEmail(email: User["email"]) {
+export async function getUserById(id: User["id"]): Promise<UserResult | null> {
+  return prisma.user.findUnique({
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+      name: true,
+      email: true,
+      profilePic: true,
+      bio: true,
+      intro: true,
+    },
+    where: { id },
+  });
+}
+
+export async function getUserByEmail(
+  email: User["email"],
+): Promise<UserResult | null> {
   return prisma.user.findUnique({ where: { email } });
 }
 
-export async function createUser(email: User["email"], password: string) {
+export async function getUserByUsername(
+  username: User["name"],
+): Promise<UserResult | null> {
+  return prisma.user.findUnique({ where: { name: username } });
+}
+
+export async function createUser(
+  email: User["email"],
+  password: string,
+  username?: string,
+) {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   return prisma.user.create({
@@ -24,6 +85,7 @@ export async function createUser(email: User["email"], password: string) {
           hash: hashedPassword,
         },
       },
+      name: username ?? email.split("@")[0],
     },
   });
 }
